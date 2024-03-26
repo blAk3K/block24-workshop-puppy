33ef0c142e1026e8ad26d3aaa53b220f87db80cb
Jessica Knott <knottfam@jessicas-imac.mynetworksettings.com>
2024-03-26 15:04:42 -0400

    origin

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,37 @@ import './App.css'
 import { useState } from 'react'
 function App() {
   const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null)
   console.log("puppyList:", puppyList);
+ 
+
+const featurePup = puppies.find((pup)=>pup.id === featPupId)
+console.log(featurePup)
 
   return (
     <>
       <div className="App">
         {
           puppies.map((puppy) => {
-            return <p key={puppy.id}>{puppy.name}</p>
-          })
-        }
+            return (
+               <p onClick= {()=>{setFeatPupId(puppy.id)}} key={puppy.id}>
+                  {puppy.name} 
+               </p>
+            ); 
+          })}
+          {featPupId && (
+            <div>
+              <h2>{featurePup.name} </h2>
+              <ul>
+                <li>
+                  age:{featurePup.age}
+                </li>
+                <li>
+                  email:{featurePup.email}
+                </li>
+              </ul>
+            </div>
+          )}
       </div>
       
     </>
